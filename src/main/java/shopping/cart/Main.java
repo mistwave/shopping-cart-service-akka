00ff1e43d6b21d2ef984d723b7e06d536ff4dded
Yifu Duan <yifu.duan@shopee.com>
2022-01-21 18:08:20 +0800

    add ShoppingCartServer

--- src/main/java/shopping/cart/Main.java
+++ src/main/java/shopping/cart/Main.java
@@ -4,8 +4,10 @@ import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
 import akka.management.javadsl.AkkaManagement;
+import com.typesafe.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import shopping.cart.proto.ShoppingCartService;
 
 public class Main {
 
@@ -24,5 +26,11 @@ public class Main {
   public static void init(ActorSystem<Void> system) {
     AkkaManagement.get(system).start();
     ClusterBootstrap.get(system).start();
+
+    Config config = system.settings().config();
+    String grpcInterface = config.getString("shopping-cart-service.grpc.interface");
+    int grpcPort = config.getInt("shopping-cart-service.grpc.port");
+    ShoppingCartService grpcService = new ShoppingCartServiceImpl();
+    ShoppingCartServer.start(grpcInterface, grpcPort, system, grpcService);
   }
 }
---
+++ src/main/java/shopping/cart/ShoppingCartServiceImpl.java
@@ -0,0 +1,25 @@
+package shopping.cart;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import shopping.cart.proto.AddItemRequest;
+import shopping.cart.proto.Cart;
+import shopping.cart.proto.Item;
+import shopping.cart.proto.ShoppingCartService;
+
+/** @author yifuduan on 2022/1/20 */
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+  public final Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Override
+  public CompletionStage<Cart> addItem(AddItemRequest in) {
+    logger.info("addItem: {} to cart {}", in.getItemId(), in.getCartId());
+    Item item = Item.newBuilder().setItemId(in.getItemId()).setQuantity(in.getQuantity()).build();
+
+    Cart cart = Cart.newBuilder().addItems(item).build();
+
+    return CompletableFuture.completedFuture(cart);
+  }
+}
